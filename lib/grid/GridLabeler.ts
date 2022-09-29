40f7286c2fb6020cbd11faa2fa077e6d09903fcb
Kevin Gilland <kgilland@caci.com>
2022-09-29 07:18:37 -0600

    chore: port of GridLabeler.java

---
+++ lib/grid/GridLabeler.ts
@@ -0,0 +1,45 @@
+import { Color } from "@ngageoint/color-js";
+import { Bounds, Labeler, PropertyConstants } from "@ngageoint/grid-js";
+import { GridLabel } from "./GridLabel";
+import { GridType } from "./GridType";
+
+/**
+ * Grid Labeler
+ * 
+ * @author osbornb
+ */
+export abstract class GridLabeler extends Labeler {
+
+    /**
+     * Default text size
+     */
+    public static readonly DEFAULT_TEXT_SIZE = MGRSProperties.getInstance()
+        .getDoubleProperty(PropertyConstants.LABELER.toString(),
+            PropertyConstants.TEXT_SIZE.toString());
+
+    /**
+     * Default buffer size
+     */
+    public static readonly DEFAULT_BUFFER = MGRSProperties.getInstance()
+        .getDoubleProperty(PropertyConstants.LABELER.toString(),
+            PropertyConstants.BUFFER.toString());
+
+    constructor(enabled: boolean, minZoom = 0, maxZoom: number | undefined, color = Color.black(), textSize = GridLabeler.DEFAULT_TEXT_SIZE, buffer = GridLabeler.DEFAULT_BUFFER) {
+        super(enabled, minZoom, maxZoom, color, textSize, buffer);
+    }
+
+    /**
+     * Get labels for the bounds
+     * 
+     * @param tileBounds
+     *            tile bounds
+     * @param gridType
+     *            grid type
+     * @param zone
+     *            grid zone
+     * @return labels
+     */
+    public abstract getLabels(tileBounds: Bounds,
+        gridType: GridType, zone: GridZone): GridLabel[];
+
+}
