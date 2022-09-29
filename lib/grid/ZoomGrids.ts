a26fc063787b02b266b16e58c18e42ffc04370f9
Kevin Gilland <kgilland@caci.com>
2022-09-29 08:04:59 -0600

    chore: port of ZoomGrids.java

---
+++ lib/grid/ZoomGrids.ts
@@ -0,0 +1,36 @@
+import { BaseZoomGrids } from "@ngageoint/grid-js";
+import { Grid } from "./Grid";
+import { GridType } from "./GridType";
+
+
+/**
+ * Zoom Level Matching Grids
+ * 
+ * @author osbornb
+ */
+export class ZoomGrids extends BaseZoomGrids<Grid> {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param zoom
+	 *            zoom level
+	 */
+	constructor(zoom: number) {
+		super(zoom);
+	}
+
+	/**
+	 * Get the grid type precision
+	 * 
+	 * @return grid type precision
+	 */
+	public  getPrecision(): GridType {
+		let type: GridType ;
+		if (super.hasGrids()) {
+			type = this.grids.begin().value.getType();
+		}
+		return type;
+	}
+
+}
