a3f3ae800c3e73f4ea40f088cc5b9608eba3253e
Kevin Gilland <kgilland@caci.com>
2022-09-30 07:14:12 -0600

    chore: port of testBandLetters from GridZonesTest.java

--- test/gzd/BandLetterRange.spec.ts
+++ test/gzd/BandLetterRange.spec.ts
@@ -2,22 +2,22 @@ import { expect } from "chai";
 import { GridZones } from '../../lib/gzd/GridZones';
 import { BandLetterRange } from '../../lib/gzd/BandLetterRange';
 
+const BAND_LETTERS = "CDEFGHJKLMNPQRSTUVWXX";
+
 /**
 * Test the full range
 */
 describe('BandLetterRange Tests', function () {
 
-    /**
-      * Test the full range
-      */
-    it('test full range', function () {
+  /**
+    * Test the full range
+    */
+  it('test full range', function () {
 
-        const BAND_LETTERS = "CDEFGHJKLMNPQRSTUVWXX";
-
-        const bandRange = new BandLetterRange();
-		for (const bandLetter of bandRange) {
-			expect((BAND_LETTERS.indexOf(bandLetter) - 10) * 8).to.be.approximately(GridZones.getSouthLatitude(bandLetter), 0.0);
-		}
-    });
+    const bandRange = new BandLetterRange();
+    for (const bandLetter of bandRange) {
+      expect((BAND_LETTERS.indexOf(bandLetter) - 10) * 8).to.be.approximately(GridZones.getSouthLatitude(bandLetter), 0.0);
+    }
+  });
 
 });
--- test/gzd/GridZones.spec.ts
+++ test/gzd/GridZones.spec.ts
@@ -1,12 +1,15 @@
 import { expect } from 'chai';
 import { GridZones } from '../../lib/gzd/GridZones';
 import { MGRSConstants } from '../../lib/MGRSConstants';
+import { MGRSUtils } from '../../lib/MGRSUtils';
+
+const BAND_LETTERS = "CDEFGHJKLMNPQRSTUVWXX";
 
 describe('GridZones Tests', function () {
 
     /**
-	 * Test zone numbers
-	 */
+     * Test zone numbers
+     */
     it('test zone numbers', function () {
         let zoneNumber = MGRSConstants.MIN_ZONE_NUMBER;
         for (let longitude = MGRSConstants.MIN_LON; longitude <= MGRSConstants.MAX_LON; longitude += MGRSConstants.ZONE_WIDTH) {
@@ -26,6 +29,32 @@ describe('GridZones Tests', function () {
 
             if (zoneNumber < MGRSConstants.MAX_ZONE_NUMBER) {
                 zoneNumber++;
+            }
+
+        }
+    });
+
+    /**
+     * Test band letters
+     */
+    it('test band letters', function () {
+        let bandLetter = MGRSConstants.MIN_BAND_LETTER;
+        for (let latitude = MGRSConstants.MIN_LAT; latitude <= MGRSConstants.MAX_LAT;
+            latitude += (latitude < 80.0 ? MGRSConstants.BAND_HEIGHT : 4.0)) {
+
+            let south = (latitude > MGRSConstants.MIN_LAT && latitude < 80.0)
+                ? MGRSUtils.previousBandLetter(bandLetter)
+                : bandLetter;
+            let north = bandLetter;
+
+            expect(BAND_LETTERS.charAt(~~Math.floor(latitude / 8 + 10))).to.equal(north);
+
+            expect(GridZones.getBandLetterFromLatitude(latitude, false)).to.equal(south);
+            expect(GridZones.getBandLetterFromLatitude(latitude, true)).to.equal(north);
+            expect(GridZones.getBandLetterFromLatitude(latitude)).to.equal(north);
+
+            if (bandLetter < MGRSConstants.MAX_BAND_LETTER) {
+                bandLetter = MGRSUtils.nextBandLetter(bandLetter);
             }
 
         }
