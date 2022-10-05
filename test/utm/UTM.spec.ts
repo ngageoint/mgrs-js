f706e33a91ab6ad39e8655782366d87cab2b31b4
Kevin Gilland <kgilland@caci.com>
2022-10-05 09:16:24 -0600

    lint: fixing lint errors

--- test/utm/UTM.spec.ts
+++ test/utm/UTM.spec.ts
@@ -3,15 +3,15 @@ import { expect } from 'chai';
 import { UTM } from '../../lib/utm/UTM';
 
 describe('UTM Tests', function () {
-    it('test to point', function () {
-        const utm = UTM.create(0, Hemisphere.NORTH, 0, 0);
-        const point = utm.toPoint();
-        expect(point).to.not.be.undefined;
-    });
+  it('test to point', function () {
+    const utm = UTM.create(0, Hemisphere.NORTH, 0, 0);
+    const point = utm.toPoint();
+    expect(point).to.not.be.undefined;
+  });
 
-    it('test from point', function () {
-        const point = Point.point(0, 0);
-        const utm = UTM.from(point);
-        expect(utm).to.not.be.undefined;
-    });
+  it('test from point', function () {
+    const point = Point.point(0, 0);
+    const utm = UTM.from(point);
+    expect(utm).to.not.be.undefined;
+  });
 });
