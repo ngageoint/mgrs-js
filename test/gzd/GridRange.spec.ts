5a581d2224a5a5351641bc83d599213f317dbd70
Kevin Gilland <kgilland@caci.com>
2022-10-05 08:22:15 -0600

    lint: fixing lint errors

--- test/gzd/GridRange.spec.ts
+++ test/gzd/GridRange.spec.ts
@@ -3,26 +3,25 @@ import { GridRange } from '../../lib/gzd/GridRange';
 import { MGRS } from '../../lib/MGRS';
 
 describe('GridRange Tests', function () {
-   
-    it('test iterator', function () {
-        let gridRange = new GridRange();
+  it('test iterator', function () {
+    let gridRange = new GridRange();
 
-        for (const zone of gridRange) {
-          let zoneNumber = zone.getNumber();
-          let bandLetter = zone.getLetter();
-    
-          let gzd = zoneNumber.toString() + bandLetter;
-          expect(MGRS.isMGRS(gzd)).to.be.true;
-          let mgrs = MGRS.parse(gzd);
-          expect(mgrs).to.not.be.null;
-          expect(mgrs.getZone()).to.be.equal(zoneNumber);
-          expect(mgrs.getBand()).to.be.equal(bandLetter);
-    
-          let point = mgrs.toPoint();
-          let southwest = zone.getBounds().getSouthwest();
-    
-          expect(southwest.getLongitude()).to.be.approximately(point.getLongitude(), 0.0001);
-          expect(southwest.getLatitude()).to.be.approximately(point.getLatitude(), 0.0001);
-        }
-    });
+    for (const zone of gridRange) {
+      let zoneNumber = zone.getNumber();
+      let bandLetter = zone.getLetter();
+
+      let gzd = zoneNumber.toString() + bandLetter;
+      expect(MGRS.isMGRS(gzd)).to.be.true;
+      let mgrs = MGRS.parse(gzd);
+      expect(mgrs).to.not.be.null;
+      expect(mgrs.getZone()).to.be.equal(zoneNumber);
+      expect(mgrs.getBand()).to.be.equal(bandLetter);
+
+      let point = mgrs.toPoint();
+      let southwest = zone.getBounds().getSouthwest();
+
+      expect(southwest.getLongitude()).to.be.approximately(point.getLongitude(), 0.0001);
+      expect(southwest.getLatitude()).to.be.approximately(point.getLatitude(), 0.0001);
+    }
+  });
 });
--- test/gzd/ZoneNumberRange.spec.ts
+++ test/gzd/ZoneNumberRange.spec.ts
@@ -3,14 +3,13 @@ import { ZoneNumberRange } from '../../lib/gzd/ZoneNumberRange';
 import { MGRSConstants } from '../../lib/MGRSConstants';
 
 describe('ZoneNumberRange Tests', function () {
-   
-    it('test iterator', function () {
-        const range = new ZoneNumberRange();
+  it('test iterator', function () {
+    const range = new ZoneNumberRange();
 
-        let count = 0;
-        for(const zoneNumber of range) {
-            count++;
-        }
-        expect(count).to.equal(MGRSConstants.MAX_ZONE_NUMBER);
-    });
+    let count = 0;
+    for (const zoneNumber of range) {
+      count++;
+    }
+    expect(count).to.equal(MGRSConstants.MAX_ZONE_NUMBER);
+  });
 });
