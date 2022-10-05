bbd7ef02d9955d35923061475526a27c251c2999
Kevin Gilland <kgilland@caci.com>
2022-10-05 06:59:12 -0600

    lint: fixing lint errors

--- test/features/GridLine.spec.ts
+++ test/features/GridLine.spec.ts
@@ -3,10 +3,10 @@ import { expect } from 'chai';
 import { GridLine } from '../../lib/features/GridLine';
 
 describe('GridLine Tests', function () {
-    it('test copy', function () {
-        const line = new Line();
-        const gridLine = new GridLine(line);
-        const gridLineCopy = gridLine.copy();
-        expect(gridLine.equals(gridLineCopy)).to.be.true;
-    });
+  it('test copy', function () {
+    const line = new Line();
+    const gridLine = new GridLine(line);
+    const gridLineCopy = gridLine.copy();
+    expect(gridLine.equals(gridLineCopy)).to.be.true;
+  });
 });
